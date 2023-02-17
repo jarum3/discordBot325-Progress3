2dbe2c75b462eab5f378bf438995f9a3251c891b
jarum3 <117324735+jarum3@users.noreply.github.com>
2023-02-17 00:33:13 -0500

    Updating deploy commands

--- deploy-commands.ts
+++ deploy-commands.ts
@@ -1,17 +1,32 @@
-module.exports = {
-  deployCommands: function () {
-    const { REST, Routes } = require('discord.js');
-    const fs = require('node:fs');
-    const path = require('node:path');
-    require('dotenv').config();
+function deployCommands() {
+  const { REST, Routes } = require('discord.js');
+  const fs = require('node:fs');
+  const path = require('node:path');
+  require('dotenv').config();
 
-    const commands = [];
-    const commandsPath = path.join(__dirname, 'commands'); // Grabbing command directory
-    const currentFileExtension = '.' + __filename.split('.').slice(-1);
+  const commands = [];
+  const commandsPath = path.join(__dirname, 'commands'); // Grabbing command directory
+  const currentFileExtension = '.' + __filename.split('.').slice(-1);
+  // Grabbing all files ending in current file extension (.ts for typescript, .js for javascript)
+  const commandFiles: string[] = fs.readdirSync(commandsPath).filter((file: string) => file.endsWith(currentFileExtension));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Push each command data to the commands list as JSON
+    if ('data' in command && 'execute' in command) {
+      commands.push(command.data.toJSON());
+    }
+    else {
+      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+  }
+  if (process.env.testing.toLowerCase() === 'true') {
+    const testingPath = path.join(__dirname, 'commands/testing'); // Grabbing testing directory
     // Grabbing all files ending in current file extension (.ts for typescript, .js for javascript)
-    const commandFiles = fs.readdirSync(commandsPath).filter((file: string) => file.endsWith(currentFileExtension));
-    for (const file of commandFiles) {
-      const filePath = path.join(commandsPath, file);
+    const testingFiles = fs.readdirSync(testingPath).filter((file: string) => file.endsWith(currentFileExtension));
+
+    for (const file of testingFiles) {
+      const filePath = path.join(testingPath, file);
       const command = require(filePath);
       // Push each command data to the commands list as JSON
       if ('data' in command && 'execute' in command) {
@@ -21,42 +36,24 @@ module.exports = {
         console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
       }
     }
-    if (process.env.testing.toLowerCase() === 'true') {
-      const testingPath = path.join(__dirname, 'commands/testing'); // Grabbing testing directory
-      // Grabbing all files ending in current file extension (.ts for typescript, .js for javascript)
-      const testingFiles = fs.readdirSync(testingPath).filter((file: string) => file.endsWith(currentFileExtension));
+  }
 
-      for (const file of testingFiles) {
-        const filePath = path.join(testingPath, file);
-        const command = require(filePath);
-        // Push each command data to the commands list as JSON
-        if ('data' in command && 'execute' in command) {
-          commands.push(command.data.toJSON());
-        }
-        else {
-          console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-        }
-      }
+  const rest = new REST({ version: '10' }).setToken(process.env.CLIENT_TOKEN);
+
+  (async () => {
+    try {
+      console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+      const data = await rest.put(
+        Routes.applicationCommands(process.env.CLIENT_ID),
+        { body: commands },
+      );
+
+      console.log(`Successfully reloaded ${data.length} application (/) commands.`);
     }
-
-    const rest = new REST({ version: '10' }).setToken(process.env.CLIENT_TOKEN);
-
-    (async () => {
-      try {
-        console.log(`Started refreshing ${commands.length} application (/) commands.`);
-
-        const data = await rest.put(
-          Routes.applicationCommands(process.env.CLIENT_ID),
-          { body: commands },
-        );
-
-        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-      }
-      catch (error) {
-        console.error(error);
-      }
-    })();
-  },
-};
-
-module.exports.deployCommands();
+    catch (error) {
+      console.error(error);
+    }
+  })();
+}
+deployCommands();
