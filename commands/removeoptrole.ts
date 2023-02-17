9b860c7c2961113b72cf7332eda8641f6c89c0f3
jarum3 <117324735+jarum3@users.noreply.github.com>
2023-02-17 17:21:54 -0500

    Adding handling for joint courses

--- commands/removeoptrole.ts
+++ commands/removeoptrole.ts
@@ -1,6 +1,5 @@
-import { OptionalRole } from '../helpers/role';
-import { getListFromFile } from '../helpers/functions';
-import { ChatInputCommandInteraction, SelectMenuComponentOptionData, SlashCommandBuilder, StringSelectMenuBuilder, ActionRowBuilder } from "discord.js";
+import { RoleSelectMenu } from '../helpers/functions';
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
 // Adds a course to the list of courses, with a role and veteran role attached
 module.exports = {
   data: new SlashCommandBuilder()
@@ -8,19 +7,8 @@ module.exports = {
     .setDescription('Provides a dropdown to remove optional roles')
     .setDefaultMemberPermissions(0),
   async execute(interaction: ChatInputCommandInteraction) {
-    const rolesList = getListFromFile('data/optroles.json') as OptionalRole[];
-    if (rolesList.length === 0) {
-      await interaction.reply({ content: 'There are no roles currently in the list.', ephemeral: true });
-      return;
-    }
-    const options: SelectMenuComponentOptionData[] = [];
-    rolesList.forEach((element: OptionalRole) => options.push({ label: element.name, description: element.description, value: element.name }));
-    const row = new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(new StringSelectMenuBuilder()
-      .setCustomId('remove-roles')
-      .setPlaceholder('Nothing selected')
-      .setMinValues(1)
-      .setMaxValues(options.length)
-      .addOptions(options));
-    await interaction.reply({ content: 'Please select which roles you\'d like to remove:', components: [row], ephemeral: true });
+    const row = await RoleSelectMenu('role-remove', true);
+    if (row) await interaction.reply({ content: 'Please select which roles you\'d like to remove:', components: [row], ephemeral: true });
+    else await interaction.reply({ content: 'There are no optional roles defined currently.', ephemeral: true })
   },
 };
