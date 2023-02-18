d2ef160880043b9676a3514395ee676872f7e2e2
jarum3 <117324735+jarum3@users.noreply.github.com>
2023-02-18 07:32:37 -0500

    Deleting categories function

---
+++ commands/testing/deletecategory.ts
@@ -0,0 +1,19 @@
+import { CategoryChannel } from 'discord.js';
+import { SlashCommandBuilder, ChatInputCommandInteraction, ChannelType } from 'discord.js';
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('deletecategory')
+    .setDescription('Takes a category from a dropdown and deletes it and all its children')
+    .addChannelOption(option =>
+      option.setName('category')
+        .setDescription('The category to delete')
+        .addChannelTypes(ChannelType.GuildCategory)
+        .setRequired(true))
+    .setDefaultMemberPermissions(0),
+  async execute(interaction: ChatInputCommandInteraction) {
+    const category: CategoryChannel = interaction.options.getChannel('category') as CategoryChannel;
+    category.children.cache.forEach(channel => { channel.delete('Deleted as part of category deletion') });
+    category.delete();
+    interaction.reply({ content: 'Deleted!', ephemeral: true });
+  },
+};
