618bd41b7e825012ffc83a54690b7067f91ee4d8
jarum3 <117324735+jarum3@users.noreply.github.com>
2023-03-09 10:52:06 -0500

    Adding help values

--- commands/testing/help.ts
+++ commands/testing/help.ts
@@ -8,10 +8,20 @@ module.exports = {
   data: new SlashCommandBuilder().setName('help').setDescription('Embeds with Pong!'),
   async execute(interaction: ChatInputCommandInteraction) {
     const embed = new EmbedBuilder()
-        .setTitle("Help")
-        .setColor(0x0099FF);
+      .setTitle('Help')
+      .setColor(0x0099FF)
+      .setDescription('These are a list of commands and a brief description of their purpose.')
+      .addFields(
+        { name: 'Add Course', value: 'Adds a course to the list of courses for this server. Creates the course role with a random color, and the veteran role with a desaturated version.\nTakes parameters video, for whether the course needs a read-only channel describing how to make videos, and joint, for if the course shares a classroom with another course.' },
+        { name: 'Add Opt Role', value: 'Adds an optional role for students to join. Creates the optional role with a random color.' },
+        { name: 'Remove Course', value: 'Removes a course from the list of courses for this server. Deletes the course role, and deletes the veteran role if no users have it.' },
+        { name: 'Remove Opt Role', value: 'Removes an optional role from the list of roles for this server. Deletes the server role.' },
+        { name: 'Select Courses Builder', value: 'Creates a dropdown for students to select roles for courses. Use this in a channel students should access to receive roles.' },
+        { name: 'Opt Roles Builder', value: 'Creates a dropdown for students to select optional roles. Use this in a channel students should access to receive roles.' },
+        { name: 'Set Semester', value: 'Sets the upcoming semester value, appended to each category' },
+        { name: 'Start Semester', value: 'Uses the currently-defined courses and optional roles to start a new semester\nAutomatically creates channels. permissions, transfers over student roles, and archives old channels.' },
+      );
 
-    const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true, embeds: [embed] });
-    interaction.reply(`Pong!\nTook ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
+    await interaction.reply({ embeds: [embed] });
   },
 };
