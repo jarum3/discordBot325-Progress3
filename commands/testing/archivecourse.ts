1ee6bef48ea585dd17fc54c4fcb1d957babaa411
jarum3 <117324735+jarum3@users.noreply.github.com>
2023-03-14 13:22:28 -0400

    Adding comments

--- commands/testing/archivecourse.ts
+++ commands/testing/archivecourse.ts
@@ -10,11 +10,11 @@ module.exports = {
     // Move course into bottom physically, remove from course list, move students into veteran roles, save that data into file
     // IE REMOVE [role id] FROM [student id]
     // TODO
-    // [ ] Move course to bottom
-    // [ ] Change role permissions
-    // [ ] Transfer student roles over
+    // [ ] Move course to bottom, find first course of name matching current semester, then find first course after that not matching, put this course above that.
+    // [ ] Change role permissions from student to veteran
+    // [ ] Transfer student roles over, loop over each student with student role for this course, remove it, add the veteran role
     // [ ] Save transferred students to a file (listed above)
-    // [ ] Transfer from current courses to previous courses (Remove category)
+    // [ ] Transfer from current courses file to previous courses file (Remove category from current course copy)
     const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
     interaction.editReply(`Pong!\nTook ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
   },
